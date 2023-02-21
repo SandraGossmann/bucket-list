a7be2d011ec0e3fc9026a6aa6a93097b568bacff
Sandra Gossmann <sandra.gossmann4@gmail.com>
2023-02-21 20:21:59 +0100

    création du WishController + fichiers twig list et show

---
+++ src/Controller/WishController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/wish', name: 'wish_')]
+class WishController extends AbstractController
+{
+    #[Route('/list', name: 'list')]
+    public function list(): Response
+    {
+        //TODO récupérer liste des souhaits
+        return $this->render('wish/list.html.twig');
+    }
+
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    public function show($id): Response
+    {
+        //TODO récupérer infos souhaits
+        return $this->render('wish/show.html.twig');
+    }
+}
