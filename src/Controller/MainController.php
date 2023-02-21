13d00752adf7b134df84965e38f7e8986842b03d
Sandra Gossmann <sandra.gossmann4@gmail.com>
2023-02-21 20:20:01 +0100

    ajout de la methode about us dans MainController.php + about.twig + maj navbar

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,14 @@ class MainController extends AbstractController
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
-
+        //TODO fonction accueil
         return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/about', name: 'main_about')]
+    public function about(): Response
+    {
+        //TODO
+        return $this->render('main/about.html.twig');
     }
 }
